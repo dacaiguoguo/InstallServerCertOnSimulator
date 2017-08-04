d069defa382171a453cde5e2351bcb60be3fc87c
sunyanguo <sunyanguo@lvmama.com>
2017-08-04 14:23:53 +0800

    Initial Commit

---
+++ InstallServerCertOnSimulator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InstallServerCertOnSimulator
+//
+//  Created by yanguo sun on 2017/8/4.
+//  Copyright © 2017年 Lvmama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
